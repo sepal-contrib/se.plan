e8a70a839a8eac1b5bd873671fc77d4bd983a41d
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-03-08 10:12:51 +0000

    add colorbar to the map

--- component/tile/map_tile.py
+++ component/tile/map_tile.py
@@ -3,6 +3,7 @@ from sepal_ui import mapping as sm
 import ipyvuetify as v
 
 from component.message import cm
+from component import parameter as cp
 
 class MapTile(sw.Tile):
     
@@ -13,6 +14,7 @@ class MapTile(sw.Tile):
         
         # create the map 
         self.m = sm.SepalMap()
+        self.m.add_colorbar(colors=cp.colors, vmin=0, vmax=5)
         
         # create a layout with 2 btn 
         self.to_asset = sw.Btn(cm.map.to_asset, class_='ma-2', disabled=True)
