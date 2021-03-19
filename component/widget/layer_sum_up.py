59a081470278e701eb534751a94d731ccd8080eb
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-03-19 15:14:02 +0000

    small description

--- component/widget/layer_sum_up.py
+++ component/widget/layer_sum_up.py
@@ -34,14 +34,19 @@ class LayerFull(v.Layout):
         ])])]
         
         rows = []
-        for i, val in enumerate(values):
+        for val, clr in zip(values, self.COLORS):
             
             if val != 0:
                 row = v.Html(tag='tr', children=[
-                    v.Html(tag='td', children=[f"{val}"]),
+                    v.Html(
+                        style_=f"color: {clr}",
+                        color= clr, 
+                        tag='td', 
+                        children=[f"{val}"]
+                    ),
                     v.Html(tag='td', children=[v.ProgressLinear(
                         v_model=int(val/total*100), 
-                        color=self.COLORS[i], 
+                        color=clr, 
                         background_opacity=0.1, 
                         height=10, 
                         rounded=True, 
@@ -65,4 +70,53 @@ class LayerFull(v.Layout):
             ]
         )
         
-#class layerPercentage()
+class layerPercentage(v.Layout):
+    
+    COLORS = cp.gradient(5) + ['grey']
+    
+    def __init__(self, layer_name, pcts=[0]*6):
+        
+        # read the layer list and find the layer information based on the layer name
+        layer_list = pd.read_csv(cp.layer_list).fillna('')
+        layer_row = layer_list[layer_list.layer_name == layer_name]
+        
+        if len(layer_row) != 1:
+            raise IndexError(f"The layer {layer_name} is not part of the existing layers of the application. Please contact our maintainer.")
+        
+        #add the title
+        title = v.Html(tag='h4', children=[layer_name])
+        # build the internal details        
+        details = v.ExpansionPanels(xs12=True, class_="mt-3", children= [
+            v.ExpansionPanel(children = [
+                v.ExpansionPanelHeader(children=['Details'], expand_icon='mdi-help-circle-outline', disable_icon_rotate=True),
+                v.ExpansionPanelContent(children=[layer_row.layer_info.values[0]])
+            ])
+        ])
+        
+        # create the list of value
+        spans = []
+        for val, clr in zip(pcts, self.COLORS):
+            if val != 0:
+                spans.append(v.Html(
+                    tag='span',
+                    class_ = 'ml-1 mr-1',
+                    style_ = f'color: {clr}',
+                    children=[f'{val}%']
+                ))
+                
+        super().__init__(
+            class_ = "ma-5",
+            row=True, 
+            children = [
+                v.Flex(xs12=True, children=[title] + spans),
+                v.Flex(xs12=True, children=[details])
+            ]
+        )
+        
+        
+            
+            
+            
+            
+        
+        
