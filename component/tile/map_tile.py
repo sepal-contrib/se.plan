94b1889f47b468c097909aa9977c0d6433e21898
John Dilger <johnjdilger@gmail.com>
2021-04-29 09:24:42 -0500

    handel draw for sub aois

--- component/tile/map_tile.py
+++ component/tile/map_tile.py
@@ -5,6 +5,8 @@ import ipyvuetify as v
 from component.message import cm
 from component import parameter as cp
 
+import geemap
+import ee
 class MapTile(sw.Tile):
     
     def __init__(self):
@@ -16,6 +18,11 @@ class MapTile(sw.Tile):
         self.m = sm.SepalMap()
         self.m.add_colorbar(colors=cp.red_to_green, vmin=0, vmax=5)
         self.m.set_drawing_controls(add=True)
+        self.m.dc.on_draw(self.handel_draw)
+        
+        # drawing managment
+        self.m.draw_features = []
+        self.m.draw_collection = None
         
         # create a layout with 2 btn 
         self.to_asset = sw.Btn(cm.map.to_asset, class_='ma-2', disabled=True)
@@ -23,7 +30,7 @@ class MapTile(sw.Tile):
         self.draw_custom_area = sw.Btn(cm.map.draw_custom_area, class_='ma-2', disabled=False)
         
         self.draw_custom_area.on_event('click',self._add_dc)
-        
+
         # create the tile
         super().__init__(
             id_ = "map_widget",
@@ -40,3 +47,16 @@ class MapTile(sw.Tile):
     def _add_dc(self, widget, data, event):
         self.m.show_dc()
         return self
+
+    def handel_draw(self, target, action, geo_json):
+
+        geom = geemap.geojson_to_ee(geo_json, False)
+        feature = ee.Feature(geom)
+        
+        if action == "deleted" and len(self.m.draw_features) > 0:
+            self.m.draw_features.remove(feature)
+        else:
+            self.m.draw_features.append(feature)
+
+        collection = ee.FeatureCollection(self.m.draw_features)
+        self.m.draw_collection = collection
