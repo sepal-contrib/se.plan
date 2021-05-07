093f606b67b94126382c0021a63310d34b81e3f4
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-05-07 06:31:39 +0000

    disable criteria was missing Fix #29

--- component/widget/constraint.py
+++ component/widget/constraint.py
@@ -97,7 +97,7 @@ class CustomPanel(v.ExpansionPanel, sw.SepalWidget):
         self.header = v.ExpansionPanelHeader(children=[cp.criteria_types[category]])
         
         # link the criterias to the select 
-        self.criterias = [c for c in criterias if c.header == category] 
+        self.criterias = [c.disable() for c in criterias if c.header == category] 
         self.select = v.Select(
             class_ = 'mt-5',
             small_chips = True,
