b18902ed118aea4be1be7b03b788b976ae6930e2
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-06-23 09:03:14 +0000

    add test for admin countries #44

--- component/parameter/file_params.py
+++ component/parameter/file_params.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
 # list of available layers for the optimisation based on the layer_list in data 
-layer_list = Path(__file__).parents[2].joinpath('utils', 'layer_list.csv')
-#layer_list = pd.read_csv().fillna('')
+layer_list = Path(__file__).parents[2]/'utils'/'layer_list.csv'
+
+# list of the lmic countries 
+country_list = Path(__file__).parents[2]/'utils'/'lmic_countries.csv'
--- component/tile/custom_aoi_tile.py
+++ component/tile/custom_aoi_tile.py
@@ -1,5 +1,8 @@
 from sepal_ui import aoi
+import pandas as pd
 import ee 
+
+from component import parameter as cp
 
 ee.Initialize()
 
@@ -15,24 +18,33 @@ class CustomAoiTile(aoi.AoiTile):
     def _check_lmic(self, change):
         """Every time a new aoi is set check if it fits the LMIC country list"""
         
-        # check if the aoi is in the LMIC
-        lmic_raster = ee.Image("projects/john-ee-282116/assets/fao-restoration/misc/lmic_global_1k")
-        aoi_ee_geom = self.view.model.feature_collection.geometry()
-
-        empt = ee.Image().byte()
-        aoi_ee_raster = empt.paint(aoi_ee_geom,1)
-
-        bit_test = aoi_ee_raster.add(lmic_raster).reduceRegion(
-            reducer=ee.Reducer.bitwiseAnd(),
-            geometry=aoi_ee_geom, 
-            scale=1000,
-            bestEffort=True,
-            maxPixels=1e13
-            )
-        # test if bitwiseAnd is 2 (1 is partial coverage, 0 no coverage)
-        test = ee.Algorithms.IsEqual(bit_test.getNumber('constant'), 2)
+        # check over the lmic country number
+        if self.view.model.admin:
+            
+            # read the country file
+            country_codes = pd.read_csv(cp.country_list).GAUL
+            included = (country_codes == self.view.w_admin_0.v_model).any()
         
-        if not test.getInfo():
+        # check if the aoi is in the LMIC
+        else:
+        
+            lmic_raster = ee.Image("projects/john-ee-282116/assets/fao-restoration/misc/lmic_global_1k")
+            aoi_ee_geom = self.view.model.feature_collection.geometry()
+
+            empt = ee.Image().byte()
+            aoi_ee_raster = empt.paint(aoi_ee_geom,1)
+
+            bit_test = aoi_ee_raster.add(lmic_raster).reduceRegion(
+                reducer=ee.Reducer.bitwiseAnd(),
+                geometry=aoi_ee_geom, 
+                scale=1000,
+                bestEffort=True,
+                maxPixels=1e13
+                )
+            # test if bitwiseAnd is 2 (1 is partial coverage, 0 no coverage)
+            included = ee.Algorithms.IsEqual(bit_test.getNumber('constant'), 2).getInfo()
+        
+        if not included:
             self.view.reset()
             raise Exception("Your AOI should be included in LMIC")
                 
