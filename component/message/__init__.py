9394e3672aafb0c5fe373510742ba9b822af2dbe
12rambau <pierrick.rambaud49@gmail.com>
2022-04-28 09:54:09 +0000

    integrate chinese translation

--- component/message/__init__.py
+++ component/message/__init__.py
@@ -3,8 +3,4 @@ from pathlib import Path
 
 from sepal_ui.translator import Translator
 
-lang = "en"
-if "CUSTOM_LANGUAGE" in os.environ:
-    lang = os.environ["CUSTOM_LANGUAGE"]
-
-cm = Translator(Path(__file__).parent, lang)
+cm = Translator(Path(__file__).parent)
