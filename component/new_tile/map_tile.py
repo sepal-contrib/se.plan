b0812d62ae45dfd254a37f3afb87bea2152654d0
12rambau <pierrick.rambaud49@gmail.com>
2023-02-13 18:39:56 +0100

    add the about section

--- component/new_tile/map_tile.py
+++ component/new_tile/map_tile.py
@@ -2,6 +2,7 @@ from sepal_ui import mapping as sm
 from sepal_ui import sepalwidgets as sw
 
 from .export_control import ExportControl
+from .about_control import AboutControl
 
 
 class MapTile(sw.Tile):
@@ -14,10 +15,12 @@ class MapTile(sw.Tile):
         full_control = sm.FullScreenControl(self.map, True, True, position="topright")
         val_control = sm.InspectorControl(self.map, False, position="bottomleft")
         export_control = ExportControl(position="bottomleft")
+        about_control = AboutControl(position="bottomleft")
 
         # add them on the map
         self.map.add(full_control)
         self.map.add(val_control)
         self.map.add(export_control)
+        self.map.add(about_control)
 
         super().__init__(id_="map_tile", title="", inputs=[self.map])
