e0485fb9060bfc281882a962c9d4c0d9f592cea1
dfguerrerom <dfgm2006@gmail.com>
2023-06-27 18:43:46 +0200

    refactor: add new components to the model

--- component/model/__init__.py
+++ component/model/__init__.py
@@ -1,2 +1,3 @@
-from .customize_layer_model import *
-from .questionnaire_model import *
+from .constraint_model import ConstraintModel  # noqa F401
+from .cost_model import CostModel  # noqa F401
+from .priority_model import PriorityModel  # noqa F401
