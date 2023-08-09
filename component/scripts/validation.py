2f294bc0126d6b81fb7b4ef738b9aede4c37fa62
dfguerrerom <dfgm2006@gmail.com>
2023-08-09 16:44:47 +0200

    feat: enhance recipe validation process

--- component/scripts/validation.py
+++ component/scripts/validation.py
@@ -9,6 +9,14 @@ from sepal_ui.sepalwidgets import TextField
 from component.parameter import recipe_schema_path
 
 
+def find_missing_property(instance, schema):
+    required_properties = schema.get("required", [])
+    for prop in required_properties:
+        if prop not in instance:
+            return prop
+    return None
+
+
 def validate_recipe(
     recipe_path: str, text_field_msg: Optional[TextField] = None
 ) -> Optional[Path]:
@@ -38,11 +46,30 @@ def validate_recipe(
 
     except ValidationError as e:
         # Constructing a path string
+        print(e)
         path = ".".join(map(str, e.path))
 
         # Check if the error is related to the signature field
-        if path == "signature":
-            error_msg = "Error: The signature is invalid."
+        if e.validator == "required":
+            # Get the path of the error
+            path_list = list(e.path)
+            # Navigate to the location of the error in the schema
+            error_schema = schema
+            for p in path_list:
+                error_schema = error_schema["properties"][p]
+            # Navigate to the location of the error in the instance data
+            error_instance = data
+            for p in path_list:
+                error_instance = error_instance[p]
+            # Find the missing property
+            missing_property = find_missing_property(error_instance, error_schema)
+            error_msg = f"Error: Missing required field '{missing_property}' in {path if path else 'root'}"
+
+        elif path == "signature":
+            error_msg = (
+                "Error: The signature is invalid, check the file is a valid recipe."
+            )
+
         else:
             # Creating a detailed error error_msg for other fields
             error_msg = f"Error in field '{path}': {e.message}"
