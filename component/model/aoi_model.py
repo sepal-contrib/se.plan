0d773159286292c0022573a3a66eb305e6ba61c0
dfguerrerom <dfgm2006@gmail.com>
2023-08-13 23:23:11 +0200

    feat: seplan_aoi improvements: AOI MODEL - new AoiModel inheriting from sepal_ui to add updated trait in the model after set the object. - replace clear attributes event to avoid remove updated trait - replace "name" as trigger trait from seplanaoi I used updated. - set map map event that will be listened by custom view and set after data is imported. - add reset method and add reset_view trait that will be listened by custom aoi view to reset widgets as well AOI VIEW - wrap parent methods to listen model instead to interact directly with the view

--- component/model/aoi_model.py
+++ component/model/aoi_model.py
@@ -1,8 +1,73 @@
 """SE.PLAN model to store the data related with areas of interest."""
 from sepal_ui import color, model
 from sepal_ui.aoi.aoi_model import AoiModel
+from sepal_ui.message import ms
 from sepal_ui.scripts import utils as su
-from traitlets import Any, Dict, Unicode, link, observe
+from traitlets import Any, Dict, Int
+
+
+class AoiModel(AoiModel):
+    updated = Int(0).tag(sync=True)
+    """announces when the model is updated"""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # set the default
+        self.set_default(self.default_vector, self.default_admin, self.default_asset)
+
+    def set_object(self, method: str = ""):
+        """Set the object (gdf/featurecollection) based on the model inputs.
+
+        The method can be manually overwritten by setting the ``method`` parameter.
+
+        Args:
+            method: a model loading method
+        """
+        # clear the model output if existing
+        self.clear_output()
+
+        # overwrite self.method
+        self.method = method or self.method
+
+        if self.method in ["ADMIN0", "ADMIN1", "ADMIN2"]:
+            self._from_admin(self.admin)
+        elif self.method == "POINTS":
+            self._from_points(self.point_json)
+        elif self.method == "SHAPE":
+            self._from_vector(self.vector_json)
+        elif self.method == "DRAW":
+            self._from_geo_json(self.geo_json)
+        elif self.method == "ASSET":
+            self._from_asset(self.asset_json)
+        else:
+            raise Exception(ms.aoi_sel.exception.no_inputs)
+
+        self.updated += 1
+
+        return self
+
+    def clear_attributes(self):
+        """Return all attributes to their default state.
+
+        Note:
+            Set the default setting as current object.
+        """
+        # keep the default
+        admin = self.default_admin
+        vector = self.default_vector
+        asset = self.default_asset
+
+        # delete all the traits but the updated one (to avoid triggering the event)
+        [setattr(self, attr, None) for attr in self.trait_names() if attr != "updated"]
+
+        # reset the outputs
+        self.clear_output()
+
+        # reset the default
+        self.set_default(vector, admin, asset)
+
+        return self
 
 
 class SeplanAoi(model.Model):
@@ -12,8 +77,11 @@ class SeplanAoi(model.Model):
     custom_layers = Dict({"type": "FeatureCollection", "features": []}).tag(sync=True)
     """dict: custom geometries drawn by the user. It's linked automatically with the map_.custom_layers"""
 
-    name = Unicode("", allow_none=True).tag(sync=True)
-    """str: given name from aoi.model to the aoi"""
+    set_map = Int(0).tag(sync=True)
+    """int: This trait is listened by the custom AOI view, and will fire the btn"""
+
+    reset_view = Int(0).tag(sync=True)
+    """int: This trait is listened by the custom AOI view, and will reset the view"""
 
     def __init__(self, **kwargs):
         # test_countries:
@@ -22,10 +90,8 @@ class SeplanAoi(model.Model):
         # Medium department: 935 (Antioquia)
         self.aoi_model = AoiModel(**kwargs)
 
-        # As the feature colleciton from model is not a trait, we need to link something that changes when model is updated, that's the name
-        link((self.aoi_model, "name"), (self, "name"))
+        self.aoi_model.observe(self.on_aoi_change, "updated")
 
-    @observe("name")
     def on_aoi_change(self, change):
         """Update the feature_collection when the aoi_model.name is updated."""
         self.feature_collection = self.aoi_model.feature_collection
@@ -51,9 +117,24 @@ class SeplanAoi(model.Model):
 
     def import_data(self, data: dict):
         """Set the data for each of the AOIs."""
+        # I need to reset the traits before importing the data
+        # so they can trigger events if they've no changed.
+        # self.reset()
+
         self.aoi_model.import_data(data["primary"])
         self.custom_layers = data["custom"]
+
+        self.set_map += 1
 
     def export_data(self):
         """Save the data from each of the AOIs."""
         return {"primary": self.aoi_model.export_data(), "custom": self.custom_layers}
+
+    def reset(self):
+        """Reset the aoi_model to its default values."""
+        self.aoi_model.clear_output()
+        self.custom_layers = {"type": "FeatureCollection", "features": []}
+
+        # I have to do this because I need to have an unique event on reset
+        # that resets the view as well
+        self.reset_view += 1
