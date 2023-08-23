f55ac643a0f540bfb56dea0748cd4579499bc7f1
dfguerrerom <dfgm2006@gmail.com>
2023-08-23 19:33:42 +0200

    feat: open info dialog first time map_tile is opened

--- component/tile/map_tile.py
+++ component/tile/map_tile.py
@@ -2,6 +2,7 @@ from sepal_ui import sepalwidgets as sw
 from sepal_ui.scripts import utils as su
 
 from component import parameter as cp
+from component.model.app_model import AppModel
 from component.model.recipe import Recipe
 from component.widget.alert_state import Alert, AlertDialog, AlertState
 from component.widget.map import SeplanMap
@@ -9,9 +10,16 @@ from component.widget.map_toolbar import MapToolbar
 
 
 class MapTile(sw.Layout):
-    def __init__(self):
+    def __init__(self, app_model: AppModel = None):
+        """Define the map tile layout.
+
+        Args:
+            app_model (AppModel, optional): The app model, it is used to comunicate this
+                map_tile with the app (like opening the info dialog when the map_tile drawer is clicked). Defaults to None.
+        """
         self._metadata = {"mount_id": "map_tile"}
         self.class_ = "d-block custom_map"
+        self.app_model = app_model
 
         super().__init__()
 
@@ -52,6 +60,18 @@ class MapTile(sw.Layout):
         build_alert.set_state("new", "map", "done")
 
         self.recipe.seplan_aoi.aoi_model.observe(self._update_aoi, "updated")
+
+        # This will open the info dialog when the map_tile drawer is clicked
+        if self.app_model:
+            self.app_model.observe(self.open_info_dialog, "active_drawer")
+
+    def open_info_dialog(self, change):
+        """Open the info dialog when the map_tile app drawer is clicked."""
+        if change["new"] == "map_tile":
+            self.map_toolbar.info_dialog.open_dialog()
+
+            # I just want to open the dialog once, so I remove the observer
+            self.app_model.unobserve(self.open_info_dialog, "active_drawer")
 
     def _update_aoi(self, *_):
         """Update the map when the aoi is updated."""
