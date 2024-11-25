462bb4fe16f02e9462f86773ce4d5fc1b0d0bf61
Daniel Guerrero <dfgm2006@gmail.com>
2024-11-25 15:35:33 +0100

    fix: fix csv result (#251)

--- component/scripts/compute.py
+++ component/scripts/compute.py
@@ -6,6 +6,13 @@ import component.parameter.gui_params as param
 from component.parameter.directory import result_dir
 from component.types import RecipeStatsDict
 
+TYPE_OF_RESULT = {
+    "benefit": "mean",
+    "constraint": "percent",
+    "cost": "sum",
+    "suitability": "sum",
+}
+
 
 def export_as_csv(recipe_summary_stats: RecipeStatsDict):
     # Function to create a multi-row format for each sub-category
@@ -17,23 +24,28 @@ def export_as_csv(recipe_summary_stats: RecipeStatsDict):
     session_results_path = (csv_folder / recipe_name).with_suffix(".csv")
 
     formatted_data = []
-    for area_data in summary_stats:
-        for area, contents in area_data.items():
-            for category, details in contents.items():
+    for recipe_name, area_stats in recipe_summary_stats.items():
+        for area_name, area_data in area_stats.items():
+            for category, details in area_data.items():
+                if category == "color":
+                    continue
+                type_result = TYPE_OF_RESULT[category]
                 if isinstance(details, list):  # For 'benefit', 'constraint', 'cost'
                     for item in details:
                         for sub_category, value in item.items():
                             row = {
-                                "Area": area,
+                                "Recipe": recipe_name,
+                                "Area": area_name,
                                 "Theme": category,
                                 "Sub-theme": sub_category,
-                                "Values": value["values"][0],
+                                "Values": value["values"][type_result],
                             }
                             formatted_data.append(row)
                 elif isinstance(details, dict):  # For 'suitability'
                     for value in details["values"]:
                         row = {
-                            "Area": area,
+                            "Recipe": recipe_name,
+                            "Area": area_name,
                             "Theme": category,
                             "Sub-theme": param.SUITABILITY_LEVELS[value["image"]],
                             "Values": value["sum"],
@@ -42,7 +54,8 @@ def export_as_csv(recipe_summary_stats: RecipeStatsDict):
                     # Adding total suitability as a separate row
                     formatted_data.append(
                         {
-                            "Area": area,
+                            "Recipe": recipe_name,
+                            "Area": area_name,
                             "Theme": "Total",
                             "Sub-theme": "Total",
                             "Values": details["total"],
