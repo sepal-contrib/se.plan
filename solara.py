d2b7b2b8e9ca81c0c64955490bebae33fd52c5bf
dfguerrerom <dfgm2006@gmail.com>
2024-12-23 20:28:57 -0500

    test

--- solara.py
+++ solara.py
@@ -1,16 +1,16 @@
-import solara_
+import solara
 from solara.lab import headers
 import ipyvuetify as v
 from sepal_ui import color
 
 
-@solara_.component
+@solara.component
 def Page():
     print(headers.value)
 
-    solara_.lab.theme.themes.light.primary = "#5BB624"
-    solara_.lab.theme.themes.dark.primary = "#76591e"
-    solara_.lab.theme.dark = color._dark_theme
+    solara.lab.theme.themes.light.primary = "#5BB624"
+    solara.lab.theme.themes.dark.primary = "#76591e"
+    solara.lab.theme.dark = color._dark_theme
 
     v.Btn.element(
         children=["this is a test", f" - theme: {headers.value}"],
