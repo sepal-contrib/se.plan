8f88984f2a2411e363b8c735fefc97d2a80b19c1
dfguerrerom <dfgm2006@gmail.com>
2025-07-04 16:05:01 -0500

    buid: aux script to lauch solara with env vars

--- run_solara.sh
+++ run_solara.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # sources the .env file and runs the Solara app
-# Usage: ./run_solara.sh [filename]
+# Usage: ./run_solara.sh [filename] [port]
 # If no filename is provided, defaults to solara_app.py
+# If no port is provided, defaults to 8900
 
 SOLARA_FILE="${1:-solara_app.py}"
+PORT="${2:-8900}"
 
 while IFS= read -r line; do
   [[ $line =~ ^#.*$ || -z $line ]] && continue
@@ -22,5 +24,5 @@ while IFS= read -r line; do
   fi
 done < .env
 
-solara run "$SOLARA_FILE" --port 8900 --no-open
-# solara run "$SOLARA_FILE" --port 8900 --no-open --log-level debug
+# solara run "$SOLARA_FILE" --port 8900 --no-open
+solara run "$SOLARA_FILE" --port $PORT --no-open --log-level debug
